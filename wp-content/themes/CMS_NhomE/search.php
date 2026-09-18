d63c75b7db3c9f4f44ef09d07edcd829d217ed5a
thanhnguyen226104 <thanhnguyen226104>
2026-09-18 10:59:56 +0700

    Tối ưu trang tìm kiếm rỗng trên di động

---
+++ wp-content/themes/CMS_NhomE/search.php
@@ -0,0 +1,12 @@
+<?php
+/** Trang tìm kiếm. */
+get_header();
+?>
+<main id="site-content">
+    <?php if ( have_posts() ) : ?>
+        <?php get_template_part( 'Module2/test' ); ?>
+    <?php else : ?>
+        <?php get_template_part( 'Module4/empty' ); ?>
+    <?php endif; ?>
+</main>
+<?php get_footer(); ?>
