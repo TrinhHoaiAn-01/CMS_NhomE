993a10a500ea885cf216ca1260c50094fde029b8
Ngocaii <huynhnhi2204vl@gmail.com>
2026-09-18 09:22:41 +0700

    Kết nối logo, tìm kiếm và menu tài khoản

--- wp-content/themes/CMS_NhomE/functions.php
+++ wp-content/themes/CMS_NhomE/functions.php
@@ -832,6 +832,7 @@ return apply_filters( 'CMS_NhomE_get_elements_array', $elements );
 
 function cms_nhome_module_assets() {
     wp_enqueue_style( 'cms-nhome-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css', array(), '4.6.2' );
+    wp_enqueue_style( 'cms-nhome-icons', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', array(), '6.4.0' );
     wp_enqueue_script( 'cms-nhome-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js', array( 'jquery' ), '4.6.2', true );
 }
 add_action( 'wp_enqueue_scripts', 'cms_nhome_module_assets' );
--- wp-content/themes/CMS_NhomE/header.php
+++ wp-content/themes/CMS_NhomE/header.php
@@ -9,15 +9,53 @@
 <?php wp_body_open(); ?>
 <header id="site-header">
     <nav class="navbar navbar-expand-lg navbar-light custom-navbar" aria-label="Điều hướng chính">
-        <a class="navbar-brand font-weight-bold" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-            <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-        </a>
+        <?php if ( has_custom_logo() ) : ?>
+            <?php the_custom_logo(); ?>
+        <?php else : ?>
+            <a class="navbar-brand font-weight-bold" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+            </a>
+        <?php endif; ?>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Mở menu">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav mr-auto align-items-lg-center">
                 <li class="nav-item"><a class="nav-link" href="<?php echo esc_url( home_url( '/' ) ); ?>">Trang chủ</a></li>
+                <li class="nav-item">
+                    <form class="form-inline" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                        <label class="sr-only" for="module-header-search">Tìm kiếm</label>
+                        <input id="module-header-search" class="form-control form-control-sm custom-search-input" type="search" name="s" value="<?php echo esc_attr( get_search_query( false ) ); ?>" placeholder="Tìm kiếm" required>
+                        <button class="btn btn-sm custom-search-btn" type="submit">Tìm</button>
+                    </form>
+                </li>
+            </ul>
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => 'primary',
+                'container'      => false,
+                'menu_class'     => 'navbar-nav cms-module-menu',
+                'fallback_cb'    => false,
+                'depth'          => 1,
+            ) );
+            ?>
+            <ul class="navbar-nav ml-lg-auto align-items-lg-center">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="accountMenu" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fa-regular fa-circle-user" aria-hidden="true"></i> Tài khoản
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="accountMenu">
+                        <?php if ( is_user_logged_in() ) : ?>
+                            <a class="dropdown-item" href="<?php echo esc_url( admin_url( 'profile.php' ) ); ?>">Hồ sơ cá nhân</a>
+                            <a class="dropdown-item" href="<?php echo esc_url( wp_logout_url( home_url( '/' ) ) ); ?>">Đăng xuất</a>
+                        <?php else : ?>
+                            <a class="dropdown-item" href="<?php echo esc_url( wp_login_url() ); ?>">Đăng nhập</a>
+                            <?php if ( get_option( 'users_can_register' ) ) : ?>
+                                <a class="dropdown-item" href="<?php echo esc_url( wp_registration_url() ); ?>">Đăng ký</a>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
+                </li>
             </ul>
         </div>
     </nav>
