49ace4ba5b213f740cd66e63713d7a785af65747
thanhnguyen226104 <thanhnguyen226104>
2026-09-23 11:51:03 +0700

    Gắn cột bài viết mới vào trang chi tiết

--- wp-content/themes/CMS_NhomE/single.php
+++ wp-content/themes/CMS_NhomE/single.php
@@ -6,6 +6,7 @@ get_header();
     <div class="fit-single-layout">
         <?php get_template_part( 'Module9/categories' ); ?>
         <?php get_template_part( 'Module6/detail' ); ?>
+        <?php get_template_part( 'Module10/recent-posts' ); ?>
     </div>
 </main>
 <?php get_footer(); ?>
